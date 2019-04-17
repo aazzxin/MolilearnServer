a570518511c11d7954d7317e9a1a9099a9c9a3b9
Bacon <33394424+aazzxin@users.noreply.github.com>
2019-04-17 10:41:11 +0800

    Update config.js

--- src/common/config/config.js
+++ src/common/config/config.js
@@ -2,8 +2,8 @@
 module.exports = {
   default_module: 'api',
   weixin: {
-    appid: 'wxf8f3215c01936c7c', // 小程序 appid
-    secret: 'b39768d0dd0b0a0e37144ee744f64904', // 小程序密钥
+    appid: '', // 小程序 appid
+    secret: '', // 小程序密钥
     mch_id: '', // 商户帐号ID
     partner_key: '', // 微信支付密钥
     notify_url: '' // 微信异步通知，例：https://www.nideshop.com/api/pay/notify
