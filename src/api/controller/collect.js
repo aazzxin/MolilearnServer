1405595cc22bbea795eb413186e68e5a0144f5f5
Bacon <aazzbing@qq.com>
2019-05-04 23:12:29 +0800

    fix card action coll value

--- src/api/controller/collect.js
+++ src/api/controller/collect.js
@@ -4,7 +4,7 @@ module.exports = class extends Base {
   async cardAction() {
     const openId = this.getLoginUserId();
     const cid = this.get('cid');
-    const coll = Boolean(this.get('coll'));
+    const coll = this.get('coll');
 
     const model = this.model('collisionCard');
 
@@ -13,11 +13,11 @@ module.exports = class extends Base {
       model.add({
         openId: openId,
         cid: cid,
-        isColl: coll ? 1 : 0,
+        isColl: coll,
         time: this.getDate()
       })
     } else {
-      model.where({openId: openId, cid: cid}).update({isColl: coll ? 1 : 0, time: this.getDate()});
+      model.where({openId: openId, cid: cid}).update({isColl: coll, time: this.getDate()});
     }
 
     return this.success();
