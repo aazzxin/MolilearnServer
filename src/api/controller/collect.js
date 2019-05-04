b2dc90969e6e9926ca470205f32a939f3b084e7b
Bacon <aazzbing@qq.com>
2019-05-04 23:33:52 +0800

    update coll count

--- src/api/controller/collect.js
+++ src/api/controller/collect.js
@@ -10,15 +10,24 @@ module.exports = class extends Base {
 
     let id = await model.where({openId: openId, cid: cid}).find();
     if (think.isEmpty(id)) {
-      model.add({
+      await model.add({
         openId: openId,
         cid: cid,
         isColl: coll,
         time: this.getDate()
       })
     } else {
-      model.where({openId: openId, cid: cid}).update({isColl: coll, time: this.getDate()});
+      await model.where({openId: openId, cid: cid}).update({isColl: coll, time: this.getDate()});
     }
+
+    // 重新计数题库收藏数，和创建者被收藏量
+    const cards = this.model('cards');
+    let collCount = await model.where({cid: cid, isColl: true}).count('*');
+    cards.where({cid: cid}).update({coll: collCount})
+    let creator = await cards.field(['openId']).where({cid: cid}).getField('openId', true);
+    
+    let userCardCollCount = await model.join('cards ON cards.cid=collisionCard.cid').where({'cards.openId': creator}).count('*');
+    this.model('usersinfo').where({openId: creator}).update({collNum: userCardCollCount});
 
     return this.success();
   }
