985840e071a8a7d3877d73055baaccc689dd0bb4
Bacon <aazzbing@qq.com>
2019-05-04 22:23:57 +0800

    add collect card action

--- src/api/controller/collect.js
+++ src/api/controller/collect.js
@@ -1,47 +1,25 @@
 const Base = require('./base.js');
 
 module.exports = class extends Base {
-  async listAction() {
-    const typeId = this.get('typeId');
+  async cardAction() {
+    const openId = this.getLoginUserId();
+    const cid = this.get('cid');
+    const coll = this.get('coll');
 
-    const list = await this.model('collect')
-      .field(['c.*', 'g.name', 'g.list_pic_url', 'g.goods_brief', 'g.retail_price'])
-      .alias('c')
-      .join({
-        table: 'goods',
-        join: 'left',
-        as: 'g',
-        on: ['c.value_id', 'g.id']
-      }).where({user_id: this.getLoginUserId(), type_id: parseInt(typeId)}).countSelect();
+    const model = this.model('collisionCard');
+
+    let id = model.where({openId: openId, cid: cid}).find();
+    if (think.isEmpty(id)) {
+      model.add({
+        openId: openId,
+        cid: cid,
+        isColl: coll,
+        time: this.getDate()
+      })
+    } else {
+      model.where({openId: openId, cid: cid}).update({isColl: coll, time: this.getDate()});
+    }
 
     return this.success(list);
-  }
-
-  async addordeleteAction() {
-    const typeId = this.post('typeId');
-    const valueId = this.post('valueId');
-
-    const collect = await this.model('collect').where({type_id: typeId, value_id: valueId, user_id: this.getLoginUserId()}).find();
-    let collectRes = null;
-    let handleType = 'add';
-    if (think.isEmpty(collect)) {
-      // 添加收藏
-      collectRes = await this.model('collect').add({
-        type_id: typeId,
-        value_id: valueId,
-        user_id: this.getLoginUserId(),
-        add_time: parseInt(new Date().getTime() / 1000)
-      });
-    } else {
-      // 取消收藏
-      collectRes = await this.model('collect').where({id: collect.id}).delete();
-      handleType = 'delete';
-    }
-
-    if (collectRes > 0) {
-      return this.success({type: handleType});
-    }
-
-    return this.fail('操作失败');
   }
 };
